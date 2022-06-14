45d60dda433be8465246c8aff94ed9831dc081d4
TangYuCheng <42094883+thongchuthanh2000@users.noreply.github.com>
2022-06-14 22:12:14 +0700

    Update env

--- src/constants/SystemConstants.js
+++ src/constants/SystemConstants.js
@@ -1,8 +1,3 @@
-export const USER_LOGIN_REQUEST = 'USER_SIGNIN_REQUEST';
-export const USER_LOGIN_SUCCESS = 'USER_SIGNIN_SUCCESS';
-export const USER_LOGIN_FAIL = 'USER_SIGNIN_FAIL';
-export const BASE_URL = "http://localhost:8080"
 export const CLIENT_ID = "AVGtKPOn8RwcwLOnNUxfosd_cbLaxW-DO3mfbZA0IZMWuwB5VQ4fMUK-ASLr2JVNwJ0otzGqmHsSl9iA"
-export const API_BASE_URL = "http://localhost:8080/api/v1";
-// export const API_BASE_URL = "https://st-school-server.herokuapp.com/api/v1";
-export const WEBSOCKET_URL = "http://localhost:8080/websocket";
+export const BASE_URL = process.env.BASE_URL || 'http://localhost:8080';
+export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080/api/v1';
