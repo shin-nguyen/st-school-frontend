4b49384da9208fbe1ec112cc88b4ef3fdf4a1791
TangYuCheng <42094883+thongchuthanh2000@users.noreply.github.com>
2022-06-15 00:52:12 +0700

    Update SystemConstants.js

--- src/constants/SystemConstants.js
+++ src/constants/SystemConstants.js
@@ -1,3 +1,3 @@
 export const CLIENT_ID = "AVGtKPOn8RwcwLOnNUxfosd_cbLaxW-DO3mfbZA0IZMWuwB5VQ4fMUK-ASLr2JVNwJ0otzGqmHsSl9iA"
-export const BASE_URL = process.env.BASE_URL || 'http://localhost:8080';
-export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080/api/v1';
+export const BASE_URL = 'https://st-school-server.herokuapp.com';
+export const API_BASE_URL = 'https://st-school-server.herokuapp.com/api/v1';
